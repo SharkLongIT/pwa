24a9d8c86ca3244262c6669b1efae7d6fae1d286
Nguyễn Thanh Long <91717013+SharkLongIT@users.noreply.github.com>
2026-03-09 11:03:42 +0700

    Update config alias

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,12 @@
-// // module.exports = {
-// //   presets: ['module:@react-native/babel-preset'],
-// // };
-
-// module.exports = {
-//   presets: ['module:@react-native/babel-preset'],
-//   plugins: [
-//     [
-//       require.resolve('babel-plugin-module-resolver'),
-//       {
-//         root: ['./src'],
-//         alias: {
-//           '~': './src',
-//         },
-//       },
-//     ],
-//     require.resolve('react-native-reanimated/plugin'),
-//   ],
-// };
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
       'module-resolver',
       {
-        root: ['./src'],
+        root: ['./'],
         alias: {
-          '~': './src',
+          '~': './src'
         },
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       },
@@ -33,3 +14,22 @@ module.exports = {
     'react-native-reanimated/plugin',
   ],
 };
+// module.exports = {
+//   presets: ['module:@react-native/babel-preset'],
+//   plugins: [
+//     [
+//       'module-resolver',
+//       {
+//         root: ['./src'],
+//         alias: {
+//           '~': './src',
+//         },
+//         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+//       },
+//     ],
+//     'react-native-reanimated/plugin',
+//   ],
+// };
+// module.exports = {
+//   presets: ['module:@react-native/babel-preset'],
+// };
